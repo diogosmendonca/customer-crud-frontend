3c599ad346458236b6fc687848530468c51b4cc5
DIOGO SILVEIRA MENDONCA <diogosmendonca@gmail.com>
2021-12-30 13:15:11 -0300

    fixing close modal do not clear data. Remove unnecessary files

--- customer-app/src/features/customer/CustomerForm.js
+++ customer-app/src/features/customer/CustomerForm.js
@@ -129,12 +129,12 @@ export default function CustomerForm(props){
     //render the component
     return(
         <>
-            <div className="modal fade" id="customerForm" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal fade" id="customerForm" tabIndex="-1" aria-labelledby="exampleModalLabel" data-bs-backdrop="static" data-bs-keyboard="false" aria-hidden="true">
                 <div className="modal-dialog">
                     <div className="modal-content">
                     <div className="modal-header">
                         <h5 className="modal-title" id="exampleModalLabel">Customer</h5>
-                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={()=>handleClose()}></button>
                     </div>
                     <div className="modal-body">
                         {customerErr.message}
